77970324da9ed897a0bd481a4096f798d81cc862
Crowdbotics <engineering@crowdbotics.com>
2023-03-01 00:09:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-resonance--66950",
+  url: "https://solitary-resonance--66950.botics.co",
+  api: "https://solitary-resonance--66950.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
